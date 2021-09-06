0d946f041177b1e4229dca4c4f8753ab4bbc1234
Avneet <avneetmaankiya@gmail.com>
2021-09-06 09:28:47 +0530

    gridview

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,28 +1,58 @@
+import 'dart:convert';
+
 import 'package:catalog_revision/models/catalog.dart';
 import 'package:catalog_revision/widgets/drawer.dart';
-import 'package:catalog_revision/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    await Future.delayed(Duration(seconds: 3));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: ListView.builder(
-  
-        padding: EdgeInsets.all(16.0),
-        itemCount: dummyList.length,
-        itemBuilder: (context, index) {
-          return ItemWidget(
-            item: dummyList[index],
-          );
-        },
-      ),
+      body: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+          ? GridView.builder(
+              itemCount: CatalogModel.items!.length,
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                final item = CatalogModel.items![index];
+                return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                    child: GridTile(child: Image.network(item.image)));
+              })
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
       drawer: MyDrawer(),
     );
   }
