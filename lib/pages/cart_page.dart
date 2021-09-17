7fa1db7e31cff58908cbe37e72dc73c66d567851
Avneet <avneetmaankiya@gmail.com>
2021-09-17 23:05:39 +0530

    placeholders

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,5 +1,6 @@
 import 'package:catalog_revision/widgets/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -7,17 +8,22 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).canvasColor,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: Text(
-          "Cart Page",
-          style: TextStyle(color: Theme.of(context).accentColor),
+        backgroundColor: Theme.of(context).canvasColor,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text(
+            "Cart Page",
+            style: TextStyle(color: Theme.of(context).accentColor),
+          ),
         ),
-      ),
-      body: Center(
-        child: Text("Cart Page"),
-      ),
-    );
+        body: Column(
+          children: [
+            Expanded(child: Placeholder().p32()),
+            Divider(
+              color: Colors.black,
+            ),
+            Placeholder().h10(context).p16(),
+          ],
+        ));
   }
 }
