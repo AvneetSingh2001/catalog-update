683b4e203bfda97cf3ce5ed3c6eb81be836220ef
Avneet <avneetmaankiya@gmail.com>
2021-09-18 00:10:17 +0530

    dummy cart

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:catalog_revision/widgets/theme.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -18,12 +19,61 @@ class CartPage extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Expanded(child: Placeholder().p32()),
+            Expanded(child: _cartList().p32()),
             Divider(
-              color: Colors.black,
-            ),
-            Placeholder().h10(context).p16(),
+              color: Theme.of(context).accentColor,
+            ).px8(),
+            _cartTotal()
           ],
         ));
+  }
+}
+
+class _cartTotal extends StatelessWidget {
+  const _cartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$9999".text.xl5.bold.color(Theme.of(context).accentColor).make(),
+          30.widthBox,
+          ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          Theme.of(context).buttonColor)),
+                  onPressed: () {},
+                  child: "Buy".text.make())
+              .w32(context)
+        ],
+      ),
+    );
+  }
+}
+
+class _cartList extends StatefulWidget {
+  const _cartList({Key? key}) : super(key: key);
+
+  @override
+  _cartListState createState() => _cartListState();
+}
+
+class _cartListState extends State<_cartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.done),
+          trailing: IconButton(
+              onPressed: () {}, icon: Icon(Icons.remove_circle_outline)),
+          title: "Item 1".text.make(),
+        );
+      },
+      itemCount: 5,
+    );
   }
 }
